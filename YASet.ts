4624f041906e02ac138d3587d9cb013769fa307d
Michael James <micrjames921@gmail.com>
2024-01-09 22:37:34 -0700

    implemented Set and tested code

---
+++ YASet.ts
@@ -0,0 +1,34 @@
+const { Collection } = require("../Collection/Collection");
+import { dataObj } from "../Collection/utils/utils";
+
+class YASet extends Collection {
+   constructor() {
+	  super();
+   }
+   has(element: any): boolean {
+	  for(let item of this) {
+		 if(item === element)
+			return true;
+	  }
+      return false;
+   }
+   add(element: any) {
+	  if(!this.has(element)) super.push(element);
+   }
+   delete(element: any) {
+	  const data = super.get();
+	  const indexes = Object.keys(data);
+	  for(const index of indexes) {
+		 if(data[index] === element) {
+			delete data[index];
+			super._size--;
+		 }
+	  }
+   }
+   clear() {
+	  for(let i = 0; i < super.size; i++)
+		 this.delete(super._data[i]);
+   }
+}
+
+exports.YASet = YASet;
---
+++ tests/index.test.ts
@@ -0,0 +1,53 @@
+const { YASet } = require("../YASet");
+
+describe("An implementation of Set.", () => {
+    describe("Basic operations.", () => {
+	    const set = new YASet();
+		const value = 1;
+		set.add(value);
+		test("Should not be empty.", () => {
+		    expect(set.get()).not.toBeFalsy();
+		});
+		test("Should only add a member once.", () => {
+		    set.add(value);
+			expect(set.size).toBe(1);
+		});
+		test("Should add the members.", () => {
+		    const data = set.get();
+			expect(data[0]).toBe(value);
+		});
+		test("Should retrieve the members.", () => {
+		    const element = set.getElement(0);
+			expect(element).toBe(value);
+		});
+		test("Should be of a given size.", () => {
+		    const size = set.size;
+			expect(size).toBe(1);
+		});
+		test("Should delete the member.", () => {
+			console.log(set.size);
+		    set.delete(1);
+			expect(set.has(1)).toBeFalsy();
+			console.log(set.size);
+		});
+		test("Should clear the members.", () => {
+		    set.clear();
+			expect(set.size).toBe(0);
+		});
+	});
+	describe("That is iterable.", () => {
+		const set = new Set();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		const setArr = [...set];
+	    test("Should be a set with 5 members.", () => {
+		    expect(setArr).toHaveLength(set.size);
+		});
+		test("Should output a set with the 5 members.", () => {
+		    expect(setArr).toEqual([1, 2, 3, 4, 5]);
+		});
+	});
+});
